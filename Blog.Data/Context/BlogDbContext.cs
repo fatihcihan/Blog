17bfc30719e789f3594714334fba29253445886a
Fatih <fatihcihan666@gmail.com>
2023-02-01 16:59:06 +0300

    Context olusturuldui migration atildi, ef icin gerekli paketler yuklendi

---
+++ Blog.Data/Context/BlogDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Blog.Entity.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Data.Context
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext()
+        {
+
+        }
+
+        public BlogDbContext(DbContextOptions<BlogDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Image> Images { get; set; }
+
+
+    }
+}
