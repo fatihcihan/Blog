5c23093eac969a57359bee75be2991a9021d59aa
Fatih <fatihcihan666@gmail.com>
2023-02-01 16:59:56 +0300

    Program classinda gerekli config. yapildi, app settings'te default connection olusturuldu

--- Blog.Web/Program.cs
+++ Blog.Web/Program.cs
@@ -1,8 +1,11 @@
+using Blog.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<BlogDbContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
